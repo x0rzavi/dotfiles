ba158073992197cccf1752a0b46934c5ab828eff
Avishek Sen <x0rzavi@gmail.com>
2022-03-08 20:47:09 +0530

    zsh4humans

---
+++ run_zsh4humans.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DIR=/home/x0rzavi/.cache/zsh4humans/
+
+if [ -d "$DIR" ]; then
+    echo -e "\nZSH4HUMANS already installed."
+else
+	if command -v curl >/dev/null 2>&1; then
+	  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
+	else
+	  sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
+	fi
+fi
