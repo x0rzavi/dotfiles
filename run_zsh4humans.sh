bb6fd0f10b30ba0dc9e61b3a1d3e5317fdc08a0d
Avishek Sen <x0rzavi@gmail.com>
2022-03-11 09:29:53 +0530

    ignore

--- run_zsh4humans.sh
+++ run_zsh4humans.sh
@@ -3,7 +3,7 @@
 DIR=/home/x0rzavi/.cache/zsh4humans/
 
 if [ -d "$DIR" ]; then
-    echo -e "\nZSH4HUMANS already installed."
+    echo -e "\nZSH4HUMANS is already installed."
 else
 	if command -v curl >/dev/null 2>&1; then
 	  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
