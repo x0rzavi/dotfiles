3918d05a8bc20efce5bbb4b91a4ac552f6edf80e
Avishek Sen <x0rzavi@gmail.com>
2022-03-19 11:30:04 +0530

    Add dot_bin/executable_kernel.sh

---
+++ dot_bin/executable_kernel.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Exit mechanism
+trap "exit" INT
+set -e
+
+# Author: https://github.com/x0rzavi
+# Description: Download my custom xanmod build from github and populate necessary directories
+# Dependencies: aria2, 7z
+
+# Variables
+directory=$(pwd)
+
+rm -fr $directory/tmpdir
+mkdir -p $directory/tmpdir
+cd $directory/tmpdir
+
+kernel_package () {
+	download_link=$(curl -s https://api.github.com/repos/x0rzavi/gentoo-kernel/releases \
+	| grep "browser_download_url.*linux.zip" | head -n 1 | cut -d : -f 2,3)
+	release_tag=$(echo $download_link | cut -d / -f 8)
+
+	mkdir -p $directory/tmpdir/$release_tag
+	echo $download_link | xargs	aria2c -x16
+	7z x "linux.zip" -o$directory/tmpdir/$release_tag
+	rm -rf linux.zip
+}
+
+kernel_install () {
+	kernel_package
+	echo -e "\n\n\nPlease input your root password to proceed for moving folder:\n"
+	#sudo mv $directory/tmpdir/$release_tag /usr/src/
+	echo -e "\nLinux Kernel Source Installation Was Successful !!"
+	echo -e "\nNow Proceed With The Usual Kernel Installation Procedure:"
+	echo -e "# make install"
+	echo -e "# make modules_install"
+	echo -e "# emerge @module-rebuild"
+	echo -e "# dracut --regenerate-all --force"
+	echo -e "# grub-mkconfig -o /boot/grub/grub.cfg"
+	#rm -rf $directory/tmpdir
+}
+
+kernel_install
