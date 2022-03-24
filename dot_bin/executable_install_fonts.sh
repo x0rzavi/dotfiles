643bfda3b98304f6dbf273d2e9a922782fb694c9
Avishek Sen <x0rzavi@gmail.com>
2022-03-24 22:53:39 +0530

    Add dot_bin/executable_install_fonts.sh Add dot_bin/executable_install_kernel.sh Add dot_bin/executable_sway_enter.sh Add dot_bin/executable_sway_exit.sh Add dot_bin/executable_sway_lock.sh Add dot_bin/x0rzavi.zsh

---
+++ dot_bin/executable_install_fonts.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Exit mechanism
+trap "exit" INT
+set -e
+
+# Author: https://github.com/x0rzavi
+# Description: Install my favourite fonts
+# Dependencies: aria2
+
+# Variables
+directory=$(pwd)
+
+rm -fr $directory/tmpdir
+mkdir -p $directory/tmpdir
+cd $directory/tmpdir
+
+apple_fonts () {
+	aria2c -x16 "https://github.com/x0rzavi/apple-fonts-nerd-patched/releases/download/v1/AppleFontsNerdPatched.7z"
+	7z x "AppleFontsNerdPatched.7z" -o$directory/tmpdir
+	rm -rf AppleFontsNerdPatched.7z
+}
+
+jetbrainsmono () {
+	mkdir -p $directory/tmpdir/JetBrainsMono
+	
+	curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
+	| grep "browser_download_url.*JetBrainsMono.zip" | cut -d : -f 2,3 \
+	| xargs aria2c -x16
+	7z x "JetBrainsMono.zip" -o$directory/tmpdir/JetBrainsMono
+	rm -rf JetBrainsMono.zip
+	
+}
+
+install_fonts () {
+	apple_fonts
+	jetbrainsmono
+	echo -e "\n\n\nPlease input your root password to proceed for moving files:\n"
+	sudo mv $directory/tmpdir/* /usr/share/fonts/
+	fc-cache --really-force --verbose
+	rm -rf $directory/tmpdir
+}
+
+install_fonts
---
+++ dot_bin/executable_sway_exit.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Author: https://github.com/x0rzavi
+# Description: Exit sway
+# Dependencies: sway
+# Theme: Catppuccin
+
+# Variables
+transparent=00000000
+black=1E1E2E
+flamingo=F2CDCD
+
+swaynag \
+	--text $flamingo \
+	--background $black \
+	--button-text $black \
+	--button-background $flamingo \
+	--border $transparent \
+	--border-bottom $transparent \
+	--border-bottom-size 0 \
+	--button-border-size 0 \
+	--button-padding 6 \
+	-t warning \
+	-f 'JetBrainsMono Nerd Font Bold 11.5' \
+	-m 'Do you really want to EXIT sway?' -B 'YES' 'swaymsg exit' -s 'NO'
