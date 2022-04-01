4ec081315d9aef54afb7196cf71464c0330b53f3
Avishek Sen <x0rzavi@gmail.com>
2022-04-01 18:54:34 +0530

    Update x0rzavi.zsh

--- dot_bin/x0rzavi.zsh
+++ dot_bin/x0rzavi.zsh
@@ -6,6 +6,7 @@
 # app-text/tldr
 # sys-apps/dust
 # duf
+# gdu
 # sys-apps/ripgrep
 # dev-util/git-delta
 # sys-apps/sd
@@ -18,19 +19,20 @@
 export MANPAGER="sh -c 'col -bx | bat -l man -p --theme Dracula'"
 
 # Extend PATH.
-path=(~/.local/bin $path)
+path=(~/.local/bin ~/.bin $path)
 
 # Export environment variables.
+export MICRO_TRUECOLOR=1
 export EDITOR=micro
 export GOBIN=$HOME/.bin
 export QT_QPA_PLATFORM=wayland
 export MOZ_ENABLE_WAYLAND=1
 export MOZ_WEBRENDER=1
-export XDG_CURRENT_DESKTOP=sway
+#export XDG_CURRENT_DESKTOP=sway
 export GTK_USE_PORTAL=0
 
 # Source additional local files if they exist.
-z4h source ~/.gh.zsh
+z4h source ~/bin/gh.zsh
 
 # Define aliases.
 alias ls='exa --icons'
@@ -38,12 +40,17 @@ alias ll='exa --icons --long --all --group --git'
 alias cat='bat --theme Dracula'
 alias diff='delta'
 alias du='dust --reverse'
+alias df='duf'
 alias find='fd --hidden'
 alias grep='rg --pretty --hidden'
 alias ps='procs'
+alias ncdu='gdu'
 
 # Zoxide
 eval "$(zoxide init --cmd cd zsh)"
+
+# Rootless Docker
+export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
 
 # Set shell options: http://zsh.sourceforge.net/Doc/Release/Options.html.
 setopt hist_ignore_all_dups	# prevent history from recording duplicated entries
