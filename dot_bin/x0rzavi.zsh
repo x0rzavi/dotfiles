5cebd65483dee24a18a76c6dfa02929011cb8b1e
Avishek Sen <x0rzavi@gmail.com>
2022-05-21 11:52:40 +0530

    Update x0rzavi.zsh

--- dot_bin/x0rzavi.zsh
+++ dot_bin/x0rzavi.zsh
@@ -1,20 +1,18 @@
 # Modern UNIX
 #
-# sys-apps/bat
-# app-misc/cheat
-# sys-apps/exa
-# app-text/tldr
-# sys-apps/dust
+# bat
+# exa
+# dust
 # duf
 # gdu
 # dog
-# sys-apps/ripgrep
-# dev-util/git-delta
-# sys-apps/sd
-# sys-apps/fd
-# app-shells/zoxide
-# sys-apps/procs
-# app-shells/fzf
+# ripgrep
+# git-delta
+# sd
+# fd
+# zoxide
+# procs
+# fzf
 
 # Export environment variables.
 export MANPAGER="sh -c 'col -bx | bat -l man -p --theme Dracula'"
@@ -28,25 +26,25 @@ export MICRO_TRUECOLOR=1
 export EDITOR=micro
 export GOBIN=$HOME/.bin
 #export QT_QPA_PLATFORM=wayland
-export MOZ_ENABLE_WAYLAND=1
-export MOZ_WEBRENDER=1
+#export MOZ_ENABLE_WAYLAND=1
+#export MOZ_WEBRENDER=1
 #export XDG_CURRENT_DESKTOP=sway
 #export GTK_USE_PORTAL=0
 
 # Source additional local files if they exist.
-z4h source ~/.bin/gh.zsh
+#z4h source ~/.bin/gh.zsh
 
 # Define aliases.
 alias ls='exa --icons'
 alias ll='exa --icons --long --all --group --git'
 alias cat='bat --theme Dracula'
 alias diff='delta'
-#alias du='dust --reverse'
+alias du='dust --reverse'
 alias df='duf'
-#alias find='fd --hidden'
+alias find='fd --hidden'
 alias grep='rg --pretty --hidden'
 alias ps='procs'
-#alias ncdu='gdu'
+alias ncdu='gdu'
 alias dig='dog'
 
 # Zoxide
