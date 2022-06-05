e8b4b235cb585a7bca20c397d3f0393e5e202fe5
Avishek Sen <x0rzavi@gmail.com>
2022-06-05 11:01:25 +0530

    Update dot_bin/executable_sway_enter.sh

--- dot_bin/executable_sway_enter.sh
+++ dot_bin/executable_sway_enter.sh
@@ -4,6 +4,7 @@
 # Description: Launch sway
 # Dependencies: sway
 # Derived: https://blog.realogs.in/getting-secrets-to-work-with-sway/
+# Derived: https://github.com/uditkarode/dots/blob/master/scripts/swaylaunch
 
 # Keyring daemon for secrets
 eval $(gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh | sed 's/^\(.*\)/export \1/g')
