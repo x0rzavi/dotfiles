31afb6faf906d871c83afe22b46e54e1b3056d3d
Avishek Sen <x0rzavi@gmail.com>
2022-07-13 23:11:52 +0530

    Update dot_bin/executable_install_fonts.sh Update dot_bin/executable_mode.sh Update dot_bin/executable_sway_enter.sh Update dot_bin/executable_x0rzavi.zsh Update dot_config/bottom/bottom.toml Update dot_config/foot/foot.ini Update dot_config/private_htop/htoprc Update dot_config/sway/config Update dot_config/waybar/style.css Update dot_zshenv.local

--- dot_bin/executable_mode.sh
+++ dot_bin/executable_mode.sh
@@ -11,34 +11,35 @@ performance () {
     ryzenadj --stapm-limit=25000 --fast-limit=30000 --slow-limit=25000 --tctl-temp=77 \
     		--vrm-current=45000 --vrmmax-current=55000 --max-performance --slow-time=75 \
     		--stapm-time=10
-    amdctl -p0 -v124
-    amdctl -p1 -v96 -f102   #Default
-    amdctl -p2 -v102 -f98   #Default
+    #amdctl -p0 -v124
+    #amdctl -p1 -v96 -f102   #Default
+    #amdctl -p2 -v102 -f98   #Default
 }
 
 battery () {
     ryzenadj --stapm-limit=15000 --fast-limit=15000 --slow-limit=15000 --tctl-temp=60
-    amdctl -p1 -v104 -f86
-    amdctl -p2 -v120 -f60
+    #amdctl -p1 -v104 -f86
+    #amdctl -p2 -v120 -f60
 }
 
 default () {
     ryzenadj --stapm-limit=15000 --fast-limit=25000 --slow-limit=20000 --tctl-temp=89 \
     		--vrm-current=35000 --vrmmax-current=45000 --slow-time=5 --stapm-time=200
-    amdctl -p0 -v53
-    amdctl -p1 -v96 -f102
-    amdctl -p2 -v102 -f98
+    #amdctl -p0 -v53
+    #amdctl -p1 -v96 -f102
+    #amdctl -p2 -v102 -f98
 }
 
 undervolt () {
-    amdctl -p0 -v124
+    #amdctl -p0 -v124
+    :
 }
 
 info () {
     echo -e "################### INFO FROM RYZENADJ ###################\n"
     ryzenadj -i
     echo -e "\n################## INFO FROM AMDCTL ###################\n"
-    amdctl -g
+    #amdctl -g
 }
 
 show_help () {
