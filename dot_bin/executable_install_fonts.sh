97d827e3bcecfb4f218ed9b81e59759db7cc71dd
Avishek Sen <x0rzavi@gmail.com>
2023-03-23 22:17:10 +0530

    Update dot_bin/executable_install_fonts.sh Update dot_config/fontconfig/fonts.conf Update dot_config/foot/foot.ini Update dot_config/rofi/config.rasi Update dot_config/waybar/config Update dot_config/waybar/style.css

--- dot_bin/executable_install_fonts.sh
+++ dot_bin/executable_install_fonts.sh
@@ -16,7 +16,7 @@ unpackdir="${directory}"/fonts_tmpdir
 cd "${unpackdir}"
 
 cleanup () {
-	set +e ; rm "${unpackdir}"/*.zip ; rm "${unpackdir}"/*.txt ; rm "${unpackdir}/*.7z" ; set -e
+	set +e ; rm "${unpackdir}"/*.zip ; rm "${unpackdir}"/*.txt ; rm "${unpackdir}"/*.7z ; set -e
 }
 
 ## Noto family fonts
@@ -28,14 +28,14 @@ noto () {
 	https://fonts.google.com/download?family=Noto%20Serif
 	EOF
 	cat <<- EOF > notosans.txt
-	NotoSans-Regular.ttf
-	NotoSans-Italic.ttf
-	# NotoSans-Medium.ttf
-	# NotoSans-MediumItalic.ttf
+	# NotoSans-Regular.ttf
+	# NotoSans-Italic.ttf
+	NotoSans-Medium.ttf
+	NotoSans-MediumItalic.ttf
 	EOF
 	cat <<- EOF > notosansmono.txt
-	static/NotoSansMono/NotoSansMono-Regular.ttf
-	# static/NotoSansMono/NotoSansMono-Medium.ttf
+	# static/NotoSansMono/NotoSansMono-Regular.ttf
+	static/NotoSansMono/NotoSansMono-Medium.ttf
 	EOF
 	cat <<- EOF > notoserif.txt
 	NotoSerif-Regular.ttf
@@ -43,7 +43,7 @@ noto () {
 	EOF
 
 	mkdir -p "${unpackdir}"/noto
-	aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}" --input-file="${unpackdir}"/noto.txt || true && echo ''
+	aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}" --input-file="${unpackdir}"/noto.txt || true && echo ''
 	7z e -bso0 -y 'Noto_Sans.zip' -i'@notosans.txt' -o"${unpackdir}"/noto
 	7z e -bso0 -y 'Noto_Sans_Mono.zip' -i'@notosansmono.txt' -o"${unpackdir}"/noto
 	7z e -bso0 -y 'Noto_Serif.zip' -i'@notoserif.txt' -o"${unpackdir}"/noto
@@ -58,28 +58,28 @@ notocjk () {
 	https://fonts.google.com/download?family=Noto%20Sans%20TC
 	EOF
 	cat <<- EOF > notosanshk.txt
-	NotoSansHK-Regular.otf
-	# NotoSansHK-Medium.otf
+	# NotoSansHK-Regular.otf
+	NotoSansHK-Medium.otf
 	EOF
 	cat <<- EOF > notosansjp.txt
-	NotoSansJP-Regular.otf
-	# NotoSansJP-Medium.otf
+	# NotoSansJP-Regular.otf
+	NotoSansJP-Medium.otf
 	EOF
 	cat <<- EOF > notosanskr.txt
-	NotoSansKR-Regular.otf
-	# NotoSansKR-Medium.otf
+	# NotoSansKR-Regular.otf
+	NotoSansKR-Medium.otf
 	EOF
 	cat <<- EOF > notosanssc.txt
-	NotoSansSC-Regular.otf
-	# NotoSansSC-Medium.otf
+	# NotoSansSC-Regular.otf
+	NotoSansSC-Medium.otf
 	EOF
 	cat <<- EOF > notosanstc.txt
-	NotoSansTC-Regular.otf
-	# NotoSansTC-Medium.otf
+	# NotoSansTC-Regular.otf
+	NotoSansTC-Medium.otf
 	EOF
 
 	mkdir -p "${unpackdir}"/notocjk
-	aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}" --input-file="${unpackdir}"/notocjk.txt || true && echo ''
+	aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}" --input-file="${unpackdir}"/notocjk.txt || true && echo ''
 	7z e -bso0 -y 'Noto_Sans_HK.zip' -i'@notosanshk.txt' -o"${unpackdir}"/notocjk
 	7z e -bso0 -y 'Noto_Sans_JP.zip' -i'@notosansjp.txt' -o"${unpackdir}"/notocjk
 	7z e -bso0 -y 'Noto_Sans_KR.zip' -i'@notosanskr.txt' -o"${unpackdir}"/notocjk
@@ -94,20 +94,20 @@ notolingual () {
 	https://fonts.google.com/download?family=Noto%20Serif%20Devanagari
 	EOF
 	cat <<- EOF > notonaskharabic.txt
-	static/NotoNaskhArabic-Regular.ttf
-	# static/NotoNaskhArabic-Medium.ttf
+	# static/NotoNaskhArabic-Regular.ttf
+	static/NotoNaskhArabic-Medium.ttf
 	EOF
 	cat <<- EOF > notoserifbengali.txt
-	static/NotoSerifBengali/NotoSerifBengali-Regular.ttf
-	# static/NotoSerifBengali/NotoSerifBengali-Medium.ttf
+	# static/NotoSerifBengali/NotoSerifBengali-Regular.ttf
+	static/NotoSerifBengali/NotoSerifBengali-Medium.ttf
 	EOF
 	cat <<- EOF > notoserifdevanagari.txt
-	static/NotoSerifDevanagari/NotoSerifDevanagari-Regular.ttf
-	# static/NotoSerifDevanagari/NotoSerifDevanagari-Medium.ttf
+	# static/NotoSerifDevanagari/NotoSerifDevanagari-Regular.ttf
+	static/NotoSerifDevanagari/NotoSerifDevanagari-Medium.ttf
 	EOF
 
 	mkdir -p "${unpackdir}"/notolingual
-	aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}" --input-file="${unpackdir}"/notolingual.txt || true && echo ''
+	aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}" --input-file="${unpackdir}"/notolingual.txt || true && echo ''
 	7z e -bso0 -y 'Noto_Naskh_Arabic.zip' -i'@notonaskharabic.txt' -o"${unpackdir}"/notolingual
 	7z e -bso0 -y 'Noto_Serif_Bengali.zip' -i'@notoserifbengali.txt' -o"${unpackdir}"/notolingual
 	7z e -bso0 -y 'Noto_Serif_Devanagari.zip' -i'@notoserifdevanagari.txt' -o"${unpackdir}"/notolingual
@@ -115,15 +115,19 @@ notolingual () {
 
 notomisc () {
 	cat <<- EOF > notomisc.txt
-	https://fonts.google.com/download?family=Noto%20Emoji
+	# https://fonts.google.com/download?family=Noto%20Emoji
+	https://fonts.google.com/download?family=Noto%20Color%20Emoji
 	https://fonts.google.com/download?family=Noto%20Sans%20Math
 	https://fonts.google.com/download?family=Noto%20Music
-	https://fonts.google.com/download?family=Noto%20Sans%20Symbols
-	https://fonts.google.com/download?family=Noto%20Sans%20Symbols%202
+	# https://fonts.google.com/download?family=Noto%20Sans%20Symbols
+	# https://fonts.google.com/download?family=Noto%20Sans%20Symbols%202
 	EOF
-	cat <<- EOF > notoemoji.txt
-	static/NotoEmoji-Regular.ttf
-	# static/NotoEmoji-Medium.ttf
+	# cat <<- EOF > notoemoji.txt
+	# static/NotoEmoji-Regular.ttf
+	# # static/NotoEmoji-Medium.ttf
+	# EOF
+	cat <<- EOF > notocoloremoji.txt
+	NotoColorEmoji-Regular.ttf
 	EOF
 	cat <<- EOF > notosansmath.txt
 	NotoSansMath-Regular.ttf
@@ -131,21 +135,22 @@ notomisc () {
 	cat <<- EOF > notomusic.txt
 	NotoMusic-Regular.ttf
 	EOF
-	cat <<- EOF > notosanssymbols.txt
-	static/NotoSansSymbols-Regular.ttf
-	# static/NotoSansSymbols-Medium.ttf
-	EOF
-	cat <<- EOF > notosanssymbols2.txt
-	NotoSansSymbols2-Regular.ttf
-	EOF
+	# cat <<- EOF > notosanssymbols.txt
+	# static/NotoSansSymbols-Regular.ttf
+	# # static/NotoSansSymbols-Medium.ttf
+	# EOF
+	# cat <<- EOF > notosanssymbols2.txt
+	# NotoSansSymbols2-Regular.ttf
+	# EOF
 
 	mkdir -p "${unpackdir}"/notomisc
-	aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}" --input-file="${unpackdir}"/notomisc.txt || true && echo ''
-	7z e -bso0 -y 'Noto_Emoji.zip' -i'@notoemoji.txt' -o"${unpackdir}"/notomisc
+	aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}" --input-file="${unpackdir}"/notomisc.txt || true && echo ''
+	# 7z e -bso0 -y 'Noto_Emoji.zip' -i'@notoemoji.txt' -o"${unpackdir}"/notomisc
+	7z e -bso0 -y 'Noto_Color_Emoji.zip' -i'@notocoloremoji.txt' -o"${unpackdir}"/notomisc
 	7z e -bso0 -y 'Noto_Sans_Math.zip' -i'@notosansmath.txt' -o"${unpackdir}"/notomisc
 	7z e -bso0 -y 'Noto_Music.zip' -i'@notomusic.txt' -o"${unpackdir}"/notomisc
-	7z e -bso0 -y 'Noto_Sans_Symbols.zip' -i'@notosanssymbols.txt' -o"${unpackdir}"/notomisc
-	7z e -bso0 -y 'Noto_Sans_Symbols_2.zip' -i'@notosanssymbols2.txt' -o"${unpackdir}"/notomisc
+	# 7z e -bso0 -y 'Noto_Sans_Symbols.zip' -i'@notosanssymbols.txt' -o"${unpackdir}"/notomisc
+	# 7z e -bso0 -y 'Noto_Sans_Symbols_2.zip' -i'@notosanssymbols2.txt' -o"${unpackdir}"/notomisc
 }
 
 ## Sans serif fonts
@@ -159,7 +164,7 @@ inter () {
 	mkdir -p "${unpackdir}"/inter
 	curl -s https://api.github.com/repos/rsms/inter/releases \
 		| grep "browser_download_url.*Inter.*.zip" | cut -d : -f 2,3 | head -n 1 \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
 	7z e -bso0 -y "Inter*.zip" -i'@inter.txt' -o"${unpackdir}"/inter && echo ''
 }
 
@@ -171,7 +176,7 @@ jost () {
 
 	mkdir -p "${unpackdir}"/jost
 	printf '%s' 'https://fonts.google.com/download?family=Jost' \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
 	7z e -bso0 -y "Jost.zip" -i'@jost.txt' -o"${unpackdir}"/jost && echo ''
 }
 
@@ -186,7 +191,7 @@ lora () {
 	mkdir -p "${unpackdir}"/lora
 	curl -s https://api.github.com/repos/cyrealtype/Lora-Cyrillic/releases/latest \
 		| grep "browser_download_url.*Lora.*.zip" | cut -d : -f 2,3 \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
 	7z e -bso0 -y "Lora*.zip" -i'@lora.txt' -o"${unpackdir}"/lora && echo ''
 }
 
@@ -198,7 +203,7 @@ bodoni () {
 
 	mkdir -p "${unpackdir}"/bodoni
 	printf '%s' 'https://fonts.google.com/download?family=Bodoni%20Moda' \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
 	7z e -bso0 -y "Bodoni*.zip" -i'@bodoni.txt' -o"${unpackdir}"/bodoni && echo ''
 }
 
@@ -211,7 +216,7 @@ baskerville () {
 
 	mkdir -p "${unpackdir}"/baskerville
 	printf '%s' 'https://fonts.google.com/download?family=Libre%20Baskerville' \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
 	7z e -bso0 -y "*Baskerville.zip" -i'@baskerville.txt' -o"${unpackdir}"/baskerville && echo ''
 }
 
@@ -224,7 +229,7 @@ caslon () {
 
 	mkdir -p "${unpackdir}"/caslon
 	printf '%s' 'https://fonts.google.com/download?family=Libre%20Caslon%20Text' \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
 	7z e -bso0 -y "*Caslon*.zip" -i'@caslon.txt' -o"${unpackdir}"/caslon && echo ''
 }
 
@@ -237,58 +242,77 @@ century_schoolbook () {
 	EOF
 
 	mkdir -p "${unpackdir}"/censcbk
-	aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}"/censcbk --input-file="${unpackdir}"/censcbk.txt || true && echo ''
+	aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}"/censcbk --input-file="${unpackdir}"/censcbk.txt || true && echo ''
 }
 
 ## Monospace fonts
 ##
 iosevka () {
 	cat <<- EOF > iosevka.txt
-	Iosevka Nerd Font Complete.ttf
-	Iosevka Nerd Font Complete Medium.ttf
+	# iosevka-regular.ttf
+	# iosevka-italic.ttf
+	iosevka-medium.ttf
+	iosevka-mediumitalic.ttf
 	EOF
 
 	mkdir -p "${unpackdir}"/iosevka
-	curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
-		| grep "browser_download_url.*Iosevka.zip" | cut -d : -f 2,3 \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
-	7z e -bso0 -y 'Iosevka.zip' -i'@iosevka.txt' -o"${unpackdir}"/iosevka && echo ''
+	curl -s https://api.github.com/repos/be5invis/Iosevka/releases/latest \
+		| grep "browser_download_url.*ttf-iosevka-[0-9].*.zip" | cut -d : -f 2,3 \
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+	7z e -bso0 -y '*iosevka*.zip' -i'@iosevka.txt' -o"${unpackdir}"/iosevka && echo ''
 }
 
 jetbrainsmono () {
 	cat <<- EOF > jetbrainsmono.txt
-	JetBrains Mono Regular Nerd Font Complete.ttf
-	JetBrains Mono Italic Nerd Font Complete.ttf
+	# fonts/ttf/JetBrainsMono-Regular.ttf
+	# fonts/ttf/JetBrainsMonoNL-Italic.ttf
+	fonts/ttf/JetBrainsMono-Medium.ttf
+	fonts/ttf/JetBrainsMono-MediumItalic.ttf
 	EOF
 
 	mkdir -p "${unpackdir}"/jetbrainsmono
-	curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
-		| grep "browser_download_url.*JetBrainsMono.zip" | cut -d : -f 2,3 \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
-	7z e -bso0 -y 'JetBrainsMono.zip' -i'@jetbrainsmono.txt' -o"${unpackdir}"/jetbrainsmono && echo ''
+	curl -s https://api.github.com/repos/JetBrains/JetBrainsMono/releases/latest \
+		| grep "browser_download_url.*JetBrainsMono-[0-9].*.zip" | cut -d : -f 2,3 \
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+	7z e -bso0 -y 'JetBrainsMono*.zip' -i'@jetbrainsmono.txt' -o"${unpackdir}"/jetbrainsmono && echo ''
 }
 
 ## Miscellaneous fonts
 ##
+nerdsyms () {
+	cat <<- EOF > nerdsyms.txt
+	Symbols-1000-em Nerd Font Complete.ttf
+	# Symbols-2048-em Nerd Font Complete.ttf
+	EOF
+
+	mkdir -p "${unpackdir}"/nerdsyms
+	curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
+		| grep "browser_download_url.*NerdFontsSymbolsOnly.zip" | cut -d : -f 2,3 \
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+	7z e -bso0 -y 'NerdFontsSymbolsOnly.zip' -i'@nerdsyms.txt' -o"${unpackdir}"/nerdsyms && echo ''
+}
+
 applefonts () {
 	cat <<- EOF > applefonts.txt
-	AppleFontsNerdPatched/NewYorkSmall-Regular.otf
-	AppleFontsNerdPatched/NewYorkSmall-RegularItalic.otf
-	AppleFontsNerdPatched/SFMono Regular Nerd Font.otf
-	AppleFontsNerdPatched/SFMono Regular Italic Nerd Font.otf
-	AppleFontsNerdPatched/SFMono Medium Nerd Font.otf
-	AppleFontsNerdPatched/SFMono Medium Italic Nerd Font.otf
-	# AppleFontsNerdPatched/SF-Pro-Text-Regular.otf
-	# AppleFontsNerdPatched/SF-Pro-Text-RegularItalic.otf
-	AppleFontsNerdPatched/SF-Pro-Text-Medium.otf
-	AppleFontsNerdPatched/SF-Pro-Text-MediumItalic.otf
+	# AppleFonts/NewYorkSmall-Medium.otf
+	# AppleFonts/NewYorkSmall-MediumItalic.otf
+	# AppleFonts/NewYorkSmall-Regular.otf
+	# AppleFonts/NewYorkSmall-RegularItalic.otf
+	# AppleFonts/SF-Mono-Regular.otf
+	# AppleFonts/SF-Mono-RegularItalic.otf
+	AppleFonts/SF-Mono-Medium.otf
+	AppleFonts/SF-Mono-MediumItalic.otf
+	# AppleFonts/SF-Pro-Text-Regular.otf
+	# AppleFonts/SF-Pro-Text-RegularItalic.otf
+	AppleFonts/SF-Pro-Text-Medium.otf
+	AppleFonts/SF-Pro-Text-MediumItalic.otf
 	EOF
 
 	mkdir -p "${unpackdir}"/applefonts
-	curl -s https://api.github.com/repos/x0rzavi/apple-fonts-nerd-patched/releases/latest \
-		| grep "browser_download_url.*.7z" | cut -d : -f 2,3 \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
-	7z e -bso0 -y 'AppleFontsNerdPatched.7z' -i'@applefonts.txt' -o"${unpackdir}"/applefonts && echo ''
+	curl -s https://api.github.com/repos/x0rzavi/apple-fonts-nerd-patched/releases \
+		| grep "browser_download_url.*AppleFonts.7z" | cut -d : -f 2,3 | head -n 1 \
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+	7z e -bso0 -y 'AppleFonts*.7z' -i'@applefonts.txt' -o"${unpackdir}"/applefonts && echo ''
 }
 
 lucide () {
@@ -299,7 +323,7 @@ lucide () {
 	mkdir -p "${unpackdir}"/lucide
 	curl -s https://api.github.com/repos/lucide-icons/lucide/releases/latest \
 		| grep "browser_download_url.*lucide-font.*.zip" | cut -d : -f 2,3 \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
 	7z e -bso0 -y "lucide-font*.zip" -i'@lucide.txt' -o"${unpackdir}"/lucide && echo ''
 }
 
@@ -315,41 +339,43 @@ recursive () {
 	mkdir -p "${unpackdir}"/recursive
 	curl -s https://api.github.com/repos/arrowtype/recursive/releases/latest \
 		| grep "browser_download_url.*ArrowType-Recursive.*.zip" | cut -d : -f 2,3 \
-		| xargs aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
+		| xargs aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 || true
 	7z e -bso0 -y "ArrowType-Recursive*.zip" -i'@recursive.txt' -o"${unpackdir}"/recursive && echo ''
 }
 
 material_symbols () {
 	cat <<- EOF > matsym.txt
-	https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsRounded%5BFILL%2CGRAD%2Copsz%2Cwght%5D.ttf
+	https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsOutlined%5BFILL%2CGRAD%2Copsz%2Cwght%5D.ttf
+	# https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsRounded%5BFILL%2CGRAD%2Copsz%2Cwght%5D.ttf
 	EOF
 
 	mkdir -p "${unpackdir}"/matsym
-	aria2c -x16 -j16 --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}"/matsym --input-file="${unpackdir}"/matsym.txt || true && echo ''
+	aria2c --console-log-level=error --summary-interval=0 --auto-file-renaming=false --max-tries=0 --dir="${unpackdir}"/matsym --input-file="${unpackdir}"/matsym.txt || true && echo ''
 }
 
 install_fonts () {
-	#noto
-	#notocjk
-	#notolingual
-	#notomisc
+	noto
+	notocjk
+	notolingual
+	notomisc
 
-	#inter
-	#jost
+	inter
+	jost
 
-	#lora
+	lora
 	#bodoni
 	#baskerville
 	#caslon
 	#century_schoolbook
 
-	#iosevka
-	#jetbrainsmono
+	iosevka
+	jetbrainsmono
 
+	nerdsyms
 	applefonts
 	#lucide
 	#recursive
-	#material_symbols
+	material_symbols
 
 	cleanup
 	if [ "$(id -u)" -eq 0 ]
