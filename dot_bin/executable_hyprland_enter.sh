f76792dc72155f49c4d10046d76a60e264d41c22
Avishek Sen <x0rzavi@gmail.com>
2023-03-26 01:16:39 +0530

    Update .bin/hyprland_enter.sh Update .config/rofi/config.rasi

--- dot_bin/executable_hyprland_enter.sh
+++ dot_bin/executable_hyprland_enter.sh
@@ -4,6 +4,7 @@
 # Description: Launch hyprland
 # Dependencies: Hyprland
 
+export WLR_DRM_NO_MODIFIERS=1 # https://github.com/swaywm/sway/issues/7025
 export GNOME_KEYRING_CONTROL="/run/user/1000/keyring"
 export SSH_AUTH_SOCK="/run/user/1000/keyring/ssh"
 
