762db00119a8b7ed04378dbf38ac33073ca357cc
Avishek Sen <x0rzavi@gmail.com>
2023-04-16 19:22:45 +0530

    Update .bin/hyprland_enter.sh Update .config/mako/config

--- dot_bin/executable_hyprland_enter.sh
+++ dot_bin/executable_hyprland_enter.sh
@@ -8,11 +8,15 @@ export XDG_CURRENT_DESKTOP=Hyprland
 export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=Hyprland
 
+export GDK_BACKEND=wayland,x11
 export SDL_VIDEODRIVER=wayland
 export CLUTTER_BACKEND=wayland
-export GDK_BACKEND=wayland
 export CLUTTER_BACKEND=wayland
 export WINIT_UNIX_BACKEND=wayland
+
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_QPA_PLATFORM="wayland;xcb"
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 
 export _JAVA_AWT_WM_NONREPARENTING=1
 export NO_AT_BRIDGE=1
