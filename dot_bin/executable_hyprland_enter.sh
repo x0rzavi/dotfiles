0277d2be7c9dc046d1f102a44f30bad4015b7b15
Avishek Sen <x0rzavi@gmail.com>
2023-04-19 09:36:48 +0530

    Update .bin/hyprland_enter.sh

--- dot_bin/executable_hyprland_enter.sh
+++ dot_bin/executable_hyprland_enter.sh
@@ -21,7 +21,9 @@ export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export _JAVA_AWT_WM_NONREPARENTING=1
 export NO_AT_BRIDGE=1
 
-export WLR_DRM_NO_MODIFIERS=1 # https://github.com/swaywm/sway/issues/7025
+# https://github.com/swaywm/sway/issues/7025
+# https://gitlab.freedesktop.org/wlroots/wlroots/-/blob/master/docs/env_vars.md
+# export WLR_DRM_NO_MODIFIERS=1
 export GNOME_KEYRING_CONTROL="/run/user/1000/keyring"
 export SSH_AUTH_SOCK="/run/user/1000/keyring/ssh"
 
