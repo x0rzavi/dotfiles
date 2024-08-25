545fabb0864fcc8843afa11e95cf3b22324b34ba
Avishek Sen <x0rzavi@gmail.com>
2024-08-25 21:14:21 +0530

    Add .config/nvim/lua/configs/conform.lua Add .config/nvim/lua/configs/lspconfig.lua Add .config/nvim/lua/options.lua Add .config/nvim/lua/plugins/init.lua

---
+++ dot_config/nvim/lua/configs/conform.lua
@@ -0,0 +1,18 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { "prettier" },
+    html = { "prettier" },
+    python = { "ruff_fix", "ruff_format" },
+    c = { "clang-format" },
+    sh = { "shfmt", "shellcheck" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+return options
---
+++ dot_config/nvim/lua/options.lua
@@ -0,0 +1,8 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
+o.relativenumber = true
+o.colorcolumn = "80"
