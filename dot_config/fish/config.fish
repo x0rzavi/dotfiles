0b45ce72d57717f82b129830765afd782ec2a36b
Avishek Sen <x0rzavi@gmail.com>
2024-09-15 11:18:13 +0530

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -20,7 +20,12 @@ set -gx npm_config_prefix "$HOME/.local"
 alias cpr="rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1"
 alias mvr="rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1 --remove-source-f
 iles"
+
+# command replacements
 alias nv="nvim"
+function cat
+	bat $argv 2> /dev/null || command cat $argv
+end
 
 if status --is-interactive
     DISPLAY=:0 SSH_ASKPASS="$HOME/.ssh/.sshkey" keychain --eval --quiet --quick id_ed25519 | source
