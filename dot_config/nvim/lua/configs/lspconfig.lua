f1842b5add2440f8374e3f086c75ccc1c8244627
Avishek Sen <x0rzavi@gmail.com>
2025-07-04 08:50:06 +0530

    Update .config/nvim/lua/configs/lspconfig.lua Update .config/nvim/lua/plugins/init.lua

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -17,7 +17,7 @@ for _, lsp in ipairs(servers) do
 end
 
 -- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
+-- lspconfig.tsserver.setup {
 --   on_attach = nvlsp.on_attach,
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
