153f7c970f669e0ff02e334a2e31d6e7f5b9e029
Avishek Sen <x0rzavi@gmail.com>
2025-08-06 11:31:07 +0530

    Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/plugins/plugins.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -27,3 +27,11 @@ end, { desc = "CMake command picker" })
 
 map("n", "<leader>rf", ":RunFile<CR>", { desc = "Code runner - Run File", noremap = true, silent = false })
 map("n", "<leader>rc", ":RunClose<CR>", { desc = "Code runner - Run Close", noremap = true, silent = false })
+
+map("n", "<leader>tm", ":Leet menu<CR>", { desc = "Leetcode menu", noremap = true, silent = false })
+map("n", "<leader>tl", ":Leet list<CR>", { desc = "Leetcode problem list", noremap = true, silent = false })
+map("n", "<leader>tr", ":Leet run<CR>", { desc = "Leetcode run", noremap = true, silent = false })
+map("n", "<leader>ts", ":Leet submit<CR>", { desc = "Leetcode submit", noremap = true, silent = false })
+map("n", "<leader>tc", ":Leet console<CR>", { desc = "Leetcode problem console", noremap = true, silent = false })
+map("n", "<leader>ti", ":Leet info<CR>", { desc = "Leetcode problem info", noremap = true, silent = false })
+map("n", "<leader>tx", ":Leet reset<CR>", { desc = "Leetcode problem reset", noremap = true, silent = false })
--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -71,6 +71,19 @@ return {
     },
   },
 
+  {
+    "kawre/leetcode.nvim",
+    -- build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+      -- include a picker of your choice, see picker section for more details
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      lang = "python3",
+    },
+  },
+
   -- {
   --   "olimorris/codecompanion.nvim",
   --   lazy = true,
