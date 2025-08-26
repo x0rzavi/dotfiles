4d7e40c3098612fcab4c4e9d18eccf0e1c30653d
Avishek Sen <x0rzavi@gmail.com>
2025-08-26 23:44:04 +0530

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,6 +3,23 @@
 -- Add any additional options here
 
 local opt = vim.opt
+local global_opt = vim.g
 opt.colorcolumn = "80"
 
-vim.g.snacks_animate = false
+global_opt.snacks_animate = false
+global_opt.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = function()
+      return vim.fn.getreg("+")
+    end,
+    ["*"] = function()
+      return vim.fn.getreg("*")
+    end,
+  },
+}
+opt.clipboard = "unnamedplus"
