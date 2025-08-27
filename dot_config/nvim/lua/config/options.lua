bf14147ebeaa8c69ba78ba4d6cf8f3c42df10dea
Avishek Sen <x0rzavi@gmail.com>
2025-08-27 09:09:45 +0530

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -10,19 +10,23 @@ global_opt.snacks_animate = false
 
 -- Only set up OSC52 clipboard if running inside Windows Terminal
 if vim.fn.environ()["WT_SESSION"] then
+  local function my_paste(reg)
+    return function(lines)
+      local content = vim.fn.getreg('"')
+      return vim.split(content, "\n")
+    end
+  end
+
   global_opt.clipboard = {
     name = "OSC 52",
     copy = {
       ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
       ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
     },
+    -- https://github.com/neovim/neovim/issues/28611
     paste = {
-      ["+"] = function()
-        return vim.fn.getreg("+")
-      end,
-      ["*"] = function()
-        return vim.fn.getreg("*")
-      end,
+      ["+"] = my_paste("+"),
+      ["*"] = my_paste("*"),
     },
   }
   opt.clipboard = "unnamedplus"
