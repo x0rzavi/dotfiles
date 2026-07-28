86a2f94b9b898787a5d0fc8e1f61e853ade5524c
Avishek Sen <x0rzavi@gmail.com>
2026-07-28 22:19:46 +0530

    Update .local/bin/update_ai_tools.sh

--- dot_local/bin/executable_update_ai_tools.sh
+++ dot_local/bin/executable_update_ai_tools.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env dash
 
-shelly install claude-code
-shelly install claude-desktop
-shelly install opencode
+## Agents
+# shelly install --upgrade claude-code
+# shelly install --upgrade claude-desktop
+# shelly install --upgrade opencode
+
+## Tools
+# cargo install --git https://github.com/rtk-ai/rtk # https://github.com/rtk-ai/rtk
+# rtk init --global
+# rtk init --global --opencode
+
+uv tool install --upgrade "headroom-ai[all]" # https://github.com/headroomlabs-ai/headroom
+headroom wrap opencode --no-project-rtk
+headroom wrap claude
+
+## Cleanup
+uv cache prune
+cargo cache --autoclean
+npm cache verify
